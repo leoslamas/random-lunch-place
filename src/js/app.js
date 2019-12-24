f458508daa3281686b931fa3f0b20d6670bd8a37
Leonardo Lamas <leoslamas@gmail.com>
2019-12-24 18:25:52 -0300

    First working

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,24 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 function teste() {
     alert("merda");
 }
+
+class Botao extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        teste();
+    }
+
+    render () {
+        return (<input type="button" onClick={this.handleClick}></input>);
+    }
+
+}
+
+ReactDOM.render(<Botao/>, document.getElementById('btn'))
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,12 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.(css)$/,
+                use: {
+                    loader: "style-loader"
+                }
             }
         ]
     },
