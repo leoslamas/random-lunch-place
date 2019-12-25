437afae1ab1eb1e871c81a8f6999e3881e8eaeb3
Leonardo Lamas <leoslamas@gmail.com>
2019-12-24 21:48:14 -0300

    Adding axios

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import '../css/style.css';
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 function teste() {
     alert("merda");
@@ -18,9 +19,35 @@ class Botao extends Component {
     }
 
     render () {
-        return (<input type="button" onClick={this.handleClick}></input>);
+        return (
+            <input type="button" onClick={this.handleClick}></input>
+        );
     }
 
 }
 
+class PersonList extends Component {
+    constructor(props){
+        super(props);
+        this.state = { persons: [] };
+    }
+
+    componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+            .then(res => {
+                const persons = res.data;
+                this.setState({ persons });
+            })
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.state.persons.map(person => <li>{person.name}</li>)}
+            </ul>
+        )
+    }
+}
+
 ReactDOM.render(<Botao/>, document.getElementById('btn'))
+ReactDOM.render(<PersonList/>, document.getElementById('list'))
