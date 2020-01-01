315e21dcb54478cbe4b99df5f568903764623543
Leonardo Lamas <leoslamas@gmail.com>
2020-01-01 04:44:23 -0300

    Preparing postgres

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 const json = `[
   {
@@ -235,6 +240,17 @@ const json = `[
 ]`;
 
 router.get('/restaurants', function(req, res, next) {
+  /*try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }*/
+  
   var obj = JSON.parse(json);
   res.send({yet: obj, already: obj});
 });
