3eb090ccefe7ba1f70b98f967e016511aada95ba
leonardo_lamas <leonardo.lamas@bemobi.com.br>
2020-01-06 18:44:19 -0300

    Database out

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
 
 const json = `[
   {
