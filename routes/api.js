7dee5d2cb965b62600c9a1186a2222a2da105558
leonardo_lamas <leonardo.lamas@bemobi.com.br>
2020-01-07 18:23:31 -0300

    Sorting. Destroying. Creating.

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,57 @@
 var express = require('express');
 var router = express.Router();
-const { Restaurants, Useds } = require('../models');
+const {
+  Restaurants,
+  Useds
+} = require('../models');
 
+//restaurants
 router.get('/restaurants', function (req, res, next) {
   var restProm = Restaurants.findAll();
   var usedsProm = Useds.findAll();
 
   Promise.all([restProm, usedsProm])
     .then(values => {
-      res.json({ yet: values[0], already: values[1] });
+      res.json({
+        yet: values[0],
+        already: values[1]
+      });
     });
 });
 
+//sort
 router.get('/sort', function (req, res, next) {
+  var restProm = Restaurants.findAll();
+  var usedsProm = Useds.findAll();
 
-  res.send({ yet: {}, already: {}, sorted: "" });
+  Promise.all([restProm, usedsProm])
+    .then(values => {
+      var today = new Date();
+      var rests = values[0];
+      var useds = values[1];
+      var lastUsed = [];
+      if (useds.length > 0) lastUsed = useds[useds.length-1];
+
+      var nextRest = rests[Math.floor(Math.random() * rests.length)];
+
+      if (today.getDate() !== lastUsed.date.getDate() && today.getMonth() !== lastUsed.date.getMonth()) {
+        Useds.create({ name: nextRest.name, date: new Date() })
+          .then(rest => {
+
+            Restaurants.destroy({ where: { name: rest.name } })
+              .then(destroyed => {
+
+                useds.push({ id: rest.id, name: rest.name, date: new Date() })
+                rests = rests.filter(item => item.id !== rest.id);
+
+                res.json({ yet: rests, already: useds, sorted: nextRest.name });
+              });
+          });
+
+      } else {
+        res.json({ yet: rests, already: useds, sorted: lastUsed.name });
+      }
+    });
 });
 
 module.exports = router;
--- src/js/components/rest-list.js
+++ src/js/components/rest-list.js
@@ -7,7 +7,7 @@ class RestList extends Component {
 
     render() {
         return (
-            <select disabled={this.props.stl==='info'} name="select" className={"border border-"+this.props.stl+" rounded"} multiple size="20">
+            <select disabled={this.props.stl==='info'} name="select" className={`border border-${this.props.stl} rounded`} multiple size="20">
                 { this.props.list.map(item => <option key={item.id} value={item.id}>{item.name}</option>) }
             </select>
         )
