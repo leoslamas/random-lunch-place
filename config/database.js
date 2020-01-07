8aadbab66d60ec2e1087001b027bbf37862abf7a
leonardo_lamas <leonardo.lamas@bemobi.com.br>
2020-01-06 21:06:26 -0300

    fixup! fixup! fixup! fixup! fixup! fixup! Sequelize

--- config/database.js
+++ config/database.js
@@ -1,7 +1,5 @@
-module.exports = {
-  username: 'root',
-  password: 'root',
-  database: 'crud_sequelize',
-  host: '127.0.0.1',
-  dialect: 'mysql',
+{
+  production: {
+    use_env_variable: "DATABASE_URL"
+  }
 }
