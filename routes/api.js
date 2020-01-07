a9875268b7905d8c6c7d11dbad87f08c930e543e
leonardo_lamas <leonardo.lamas@bemobi.com.br>
2020-01-06 22:37:58 -0300

    fixup! fixup! fixup! fixup! Get all results

--- routes/api.js
+++ routes/api.js
@@ -16,7 +16,7 @@ router.get('/restaurants', function (req, res, next) {
       used = result;
     });
 
-  await Promise.all([r, u]);
+  Promise.all([r, u]);
 
   res.send({ yet: rest, already: used });
 });
