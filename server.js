e63ddda348ce3f682f61d70a269d5b1a3051ffb9
leonardo_lamas <leonardo.lamas@bemobi.com.br>
2020-01-07 18:29:22 -0300

    Fixing favicon

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var favicon = require('serve-favicon');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -13,12 +14,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(favicon());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.favicon());
 
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
