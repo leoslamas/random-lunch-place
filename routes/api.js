bd4bd9c9b71902b2cd2323bcd6bc1f49909f7770
leonardo_lamas <leonardo.lamas@bemobi.com.br>
2020-01-06 22:09:13 -0300

    Showing db restaurants

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,10 @@ var router = express.Router();
 const { Restaurants, Used } = require('../app/models');
 
 router.get('/restaurants', function(req, res, next) {
+  var rest = await Restaurants.findAll();
+  var used = await Used.findAll();
 
-  res.send({yet: {}, already: {}});
+  res.send({yet: rest, already: used});
 });
 
 router.get('/sort', function (req, res, next) {
