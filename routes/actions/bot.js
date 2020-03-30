ec3f2ae0faee6ea1e6ffaa61719758003bcb8aae
leonardo_lamas <leonardo.lamas@bemobi.com.br>
2020-03-30 14:43:04 -0300

    Adicionando Card

--- routes/actions/bot.js
+++ routes/actions/bot.js
@@ -3,15 +3,24 @@ module.exports = (req, res, next) => {
     console.log("Bot Body:");
     console.log(req.body);
 
-    if(req.body.type === "MESSAGE") {
+    if (req.body.type === "MESSAGE") {
         var temp = res.json;
-        res.json = function(json) {
-            json = { 
-                text: `Today's lunch place: *${json.sorted}*!`
+        res.json = function (json) {
+            json = {
+                "cards": [{
+                    "sections": [{
+                        "widgets": [{
+                            "textParagraph": {
+                                "text": `<center>Today's lunch place: <b>${json.sorted}</b>!</center>`
+                            }
+                        }]
+                    }]
+                }]
             };
-            temp.apply(this,arguments);
+
+            temp.apply(this, arguments);
         }
-        
+
         next();
 
     } else {
